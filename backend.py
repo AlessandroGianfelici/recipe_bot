9cac51cf13dff8542e144cbd9305cde671a0cc59
Gianfelici <gianfea@CORP.EDISON.IT>
2022-01-22 19:51:12 +0100

    minor changes

--- backend.py
+++ backend.py
@@ -7,10 +7,12 @@ from bot_token import bot_token
 
 def handle(msg):
     chat_id = msg['chat']['id']
+    print(msg)
     if 'text' in msg.keys():
         bot.sendMessage(chat_id, f"Give me the list of the ingredients you have, separated by comma!")
         
     return None
+
 
 bot = telepot.Bot(bot_token)
 bot.message_loop(handle)
